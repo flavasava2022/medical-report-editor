ce603d47362e8a7070d66e5ed5a90e4d4305f680
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2025-09-05 23:58:48 +0300

    fix responsive bug

--- src/features/patients/PatientList.jsx
+++ src/features/patients/PatientList.jsx
@@ -13,7 +13,7 @@ export default function PatientList({ patients, onSelectPatient,handleAddPatient
 
   return (
     <main className="max-w-7xl mx-auto p-4 md:p-8">
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-center mb-6 flex-wrap gap-4">
         <h2 className="text-2xl font-bold text-gray-700">Patient List</h2>
 
         <div className="w-full max-w-xs flex gap-2 ">
